98c592d82da761285018298c68263b9489ef5607
d21927790d <dedepankeke@gmail.com>
2023-03-06 14:35:20 +0100

    Practica 2 terminada.

--- vj_practica2/src_practica_02/practica_02_base.js
+++ vj_practica2/src_practica_02/practica_02_base.js
@@ -83,45 +83,56 @@ const green =		[0.0, 1.0, 0.0, 1.0];
 const blue =		[0.0, 0.0, 1.0, 1.0];
 const lightred =	[1.0, 0.5, 0.5, 1.0];
 const lightgreen =	[0.5, 1.0, 0.5, 1.0];
+
+const darkgreen = 	[6/255, 114/255, 60/255, 1.0];
 const lightblue = 	[0.5, 0.5, 1.0, 1.0];
+const darkblue = 	[10/255, 6/255, 114/255, 1.0];
+const greenblue =	[27/255, 165/255, 119/255, 1.0];
 const white =		[1.0, 1.0, 1.0, 1.0];
+
+const ligthyellow = [227/255, 239/255, 112/255, 1.0];
+const yellow =		[247/255, 219/255, 5/255, 1.0];
+const pink = 		[240/255, 138/255, 234/255, 1.0];
+const purple = 		[146/255, 11.0, 138/255, 1.0];
+const darkpurple =	[91/255, 22/255, 149/255, 1.0];
+
+const orange = 		[254/255, 116/255, 5/255, 1.0];
+const brown =		[133/255, 75/255, 12/255, 1.0];
+const grey = 		[198/255, 214/255, 200/255, 1.0];
+const darkred = 	[163/255, 7/255, 7/255, 1.0];
+const darkyellow = 	[148/255, 160/255, 11/255, 1.0];
 
 const colorsAxes = [
 	green, green, //x
 	red, red,     //y
 	blue, blue,   //z
-];	
-
-const colorsWireCube = [
-	white, white, white, white, white,
-	white, white, white, white, white,
-	white, white, white, white, white,
-	white, white, white, white, white,
-	white, white, white, white, white,
-	white, white, white, white, white,
 ];
 
-const colorsCube = [	
-	lightblue, lightblue, lightblue, lightblue, lightblue, lightblue,
-	lightgreen, lightgreen, lightgreen, lightgreen, lightgreen, lightgreen,
-	lightred, lightred, lightred, lightred, lightred, lightred,
-	blue, blue, blue, blue, blue, blue,
-	red, red, red, red, red, red,
-	green, green, green, green, green, green,
-];	
+const colorsWireCube = white;
+
+const colorsCube = [red, green, blue, lightred, lightgreen, darkgreen, lightblue, 
+	darkblue, greenblue, white, ligthyellow, yellow, pink, purple, darkpurple, orange, brown, grey, darkred, darkyellow];
 
 //----------------------------------------------------------------------------
 // CAMARA DATA
 //----------------------------------------------------------------------------
-var camera_mov    = 0.1;   // Camera speed movement.
-var aspect_ratio  = 0.0;   // Canvas aspect ratio.
-var now_FOV   	  = 45.0;  // Current FOV.
-var min_FOV   	  = 10.0;  // Mininum FOV.
-var max_FOV   	  = 170.0; // Maximum FOV.
-var near_clip 	  = 0.1;   // Near clip.
-var far_clip  	  = 100.0; // Far clip.
-var has_perspective = true;
-
+var camera_mov      = 0.1;   // Camera speed movement.
+var aspect_ratio    = 0.0;   // Canvas aspect ratio.
+var current_fov     = 45.0;  // Current FOV.
+var min_fov   	    = 10.0;  // Mininum FOV.
+var max_fov   	    = 170.0; // Maximum FOV.
+var near_clip 	    = 0.1;   // Near clip.
+var far_clip  	    = 100.0; // Far clip.
+var has_perspective = true;  // The camera is in perspective projection.
+var previous_x 		= 0.0;   // Previous x position before moving the camera again.
+var previous_y      = 0.0;   // Previous y position before moving the camera again.
+var can_rotate		= false; 
+var is_rotating     = false; // The camera is rotating.
+var rotating_factor = 0.05;	 // Distance to degrees.
+var current_pitch   = 0.0;   // Current rotation in X-axis.
+var current_yaw		= 0.0;   // Current rotation in Y-axis.
+var max_pitch 	 	= 90.0;  // Rotation in X-axis.
+var max_yaw   	 	= 90.0;  // Rotation in Y-axis.
 //----------------------------------------------------------------------------
 // OTHER DATA 
 //----------------------------------------------------------------------------
@@ -133,14 +144,13 @@ var projection = new mat4();	// create a projection matrix and set it to the ide
 var eye, target, up;			// for view matrix
 
 var rotAngle = 0.0;
-var rotChange = 12.5;
+var rotChange = 0.5;
 
 var program;
 var uLocations = {};
 var aLocations = {};
 
-var lastFrameTime = 0.0;
-var deltaTime = 0.0;
+var timer;
 
 var programInfo = {
 			program,
@@ -168,38 +178,49 @@ var objectsToDraw = [
 			u_model: new mat4(),
 		  },
 		  primType: "line_strip",
-		},	
-		{
-		  programInfo: programInfo,
-		  pointsArray: pointsCube, 
-		  colorsArray: colorsCube, 
-		  uniforms: {
+		},
+];
+
+for (let i=0; i < 20; i++)
+{
+	var cube = {
+		programInfo: programInfo,
+		pointsArray: pointsCube,
+		colorsArray: colorsCube[i], 
+		uniforms: {
 			u_colorMult: [1.0, 1.0, 1.0, 1.0],
 			u_model: new mat4(),
-		  },
-		  primType: "triangles",
-		},		
-		{
-		  programInfo: programInfo,
-		  pointsArray: pointsCube, 
-		  colorsArray: colorsCube, 
-		  uniforms: {
-			u_colorMult: [0.5, 0.5, 0.5, 1.0],
-			u_model: new mat4(),
-		  },
-		  primType: "triangles",
-		},		
-		{
-		  programInfo: programInfo,
-		  pointsArray: pointsCube, 
-		  colorsArray: colorsCube, 
-		  uniforms: {
-			u_colorMult: [0.5, 0.5, 0.5, 1.0],
-			u_model: new mat4(),
-		  },
-		  primType: "triangles",
-		}					
-];
+		},
+		primType: "triangles",
+	  };
+	objectsToDraw.push(cube);
+}
+
+function product(m, v) {
+	let r = vec4(0.0, 0.0, 0.0, 0.0);
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			r[i] += m[i][j] * v[j];
+		}
+	}
+	return r;
+}
+
+var ejes = [];
+function random_array() {
+	ejes.push(vec3());
+	ejes.push(vec3());
+	for (let i = 2; i < 22; i++) {
+		let choose = Math.floor(Math.random() * 3);
+		if (choose == 0) {
+			ejes.push(vec3(1.0, 0.0, 0.0));
+		} else if (choose == 1) {
+			ejes.push(vec3(0.0, 1.0, 0.0));
+		} else {
+			ejes.push(vec3(0.0, 0.0, 1.0));
+		}
+	}
+}
 
 //----------------------------------------------------------------------------
 // Initialization function
@@ -209,8 +230,6 @@ window.onload = function init() {
 	
 	// Set up a WebGL Rendering Context in an HTML5 Canvas
 	var canvas = document.getElementById("gl-canvas");
-	aspect_width = canvas.width;
-	aspect_height = canvas.height;
 	aspect_ratio = canvas.width/canvas.height;
 	gl = WebGLUtils.setupWebGL(canvas);
 	if (!gl) {
@@ -246,18 +265,92 @@ window.onload = function init() {
 
 	// Set up camera
 	// Projection matrix
-	projection = perspective( now_FOV, aspect_ratio, near_clip, far_clip );
+	projection = perspective(current_fov, aspect_ratio, near_clip, far_clip);
 	gl.uniformMatrix4fv( programInfo.uniformLocations.projection, gl.FALSE, projection ); // copy projection to uniform value in shader
     // View matrix (static cam)
 	eye = vec3(-5.0, 5.0, 10.0);
 	target = vec3(0.0, 0.0, 0.0);
-	up =  vec3(0.0, 1.0, 0.0);
+	up =  vec3(0, 1.0, 0);
 	view = lookAt(eye,target,up);
 	gl.uniformMatrix4fv(programInfo.uniformLocations.view, gl.FALSE, view); // copy view to uniform value in shader
 	
+	canvas.addEventListener('mousedown', (event) => {
+		if (event.button === 0) {
+			previous_x = event.clientX;
+			previous_y = event.clientY;
+			can_rotate = true;
+		}
+	})
+
+	canvas.addEventListener("mouseup", (event) => {
+		if (event.button === 0) {
+			previous_x = 0;
+			previous_y = 0;
+			current_pitch = 0;
+			current_yaw   = 0;
+			can_rotate = false;
+		}
+	});
+
+	canvas.addEventListener("mousemove", (event) => {
+		if (can_rotate) {
+			if (Math.abs(current_pitch) < max_pitch) {
+				let aux_y = event.clientY;
+				let current_y = (event.clientY - previous_y) * rotating_factor;
+				previous_y    = aux_y;
+				current_pitch += current_y;
+				let cos_x = Math.cos(current_y * (Math.PI/180.0)),
+					sin_x = Math.sin(current_y * (Math.PI/180.0)),
+					rx    = [
+						[1.0, 0.0  ,  0.0  , 0.0],
+						[0.0, cos_x, -sin_x, 0.0],
+						[0.0, sin_x,  cos_x, 0.0],
+						[0.0, 0.0  ,  0.0  , 1.0]
+					];
+				let c = vec4(target[0]-eye[0], target[1]-eye[1], target[2]-eye[2], 1.0);
+				let h = product(rx, c);
+				target = vec3(h[0]+eye[0], h[1]+eye[1], h[2]+eye[2]);
+				//c  = vec4(up[0], up[1], up[2], 0.0);
+				//h  = product(rx, c);
+				//up = vec3(h[0], h[1], h[2]);
+			}
+			if (Math.abs(current_yaw) < max_yaw) {
+				let aux_x     = event.clientX;
+				let current_x = (event.clientX - previous_x) * rotating_factor;
+				previous_x    = aux_x;
+				current_yaw  += current_x;
+				let cos_y = Math.cos(current_x * (Math.PI/180.0)),
+					sin_y = Math.sin(current_x * (Math.PI/180.0)),
+					ry    = [
+						[ cos_y, 0.0, sin_y, 0.0],
+						[ 0.0  , 1.0, 0.0  , 0.0],
+					    [-sin_y, 0.0, cos_y, 0.0],
+					    [ 0.0  , 0.0, 0.0  , 1.0]
+					];
+				let c  = vec4(target[0]-eye[0], target[1]-eye[1], target[2]-eye[2], 1.0);
+				let h = product(ry, c);
+				target = vec3(h[0]+eye[0], h[1]+eye[1], h[2]+eye[2]);	
+				c  = vec4(up[0], up[1], up[2], 0.0);
+				h  = product(ry, c);
+				up = vec3(h[0], h[1], h[2]);
+			}
+			view = lookAt(eye, target, up);
+			gl.uniformMatrix4fv(programInfo.uniformLocations.view, gl.FALSE, view);
+		}
+	});
+
+	random_array();
+	timer = setTimeout(random_color, 1000);
 	requestAnimFrame(render);
   
 };
+
+function random_color() {
+	clearTimeout(timer);
+	timer = setTimeout(random_color, 1000);
+	gl.clearColor(Math.random(), Math.random(), Math.random(), 1);
+
+}
 
 //----------------------------------------------------------------------------
 // Camera movement
@@ -266,6 +359,7 @@ window.onload = function init() {
 // - https://www.gavsblog.com/blog/detect-single-and-multiple-keypress-events-javascript
 // - https://www.toptal.com/developers/keycode
 // - https://stackoverflow.com/questions/24147546/webgl-orthographic-camera
+// - https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
 let keys_pressed = {};
 document.addEventListener('keydown', (event) => {
 	if (event.key == 'ArrowLeft' || event.key == 'ArrowUp' || event.key == 'ArrowRight' || event.key == 'ArrowDown' || event.key == 'o' || event.key == 'p' || event.key == '+' || event.key == '-') {
@@ -322,23 +416,24 @@ function camera() {
 	// FOV
 	// '+': more fov:
 	if (keys_pressed['+']) { 
-		if (now_FOV < max_FOV) {
+		if (current_fov < max_fov) {
 			console.log("More FOV!");
-			now_FOV += 5;
+			current_fov += 5;
 		}
 	}
 	// '-': less fov:
 	else if (keys_pressed['-']) { 
-		if (now_FOV > min_FOV) {
+		if (current_fov > min_fov) {
 			console.log("Less FOV?");
-			now_FOV -= 5;
+			current_fov -= 5;
 		}
 	}
 	if (!has_perspective) {
-		projection = ortho(-aspect_ratio * now_FOV * 0.1, aspect_ratio * now_FOV * 0.1, -now_FOV * 0.1, now_FOV * 0.1, near_clip, far_clip);
+		projection = ortho(-aspect_ratio * current_fov * 0.1, aspect_ratio * current_fov * 0.1, -current_fov * 0.1, current_fov * 0.1, near_clip, far_clip);
 	} else {
-		projection = perspective( now_FOV, aspect_ratio, near_clip, far_clip);
+		projection = perspective(current_fov, aspect_ratio, near_clip, far_clip);
 	}
+
 	gl.uniformMatrix4fv(programInfo.uniformLocations.projection, gl.FALSE, projection);
 	gl.uniformMatrix4fv(programInfo.uniformLocations.view, gl.FALSE, view);
 }
@@ -349,25 +444,26 @@ function camera() {
 
 function render() {
 
-	let currFrameTime = performance.now();
-	deltaTime = currFrameTime - lastFrameTime;
 	gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 	
 	//----------------------------------------------------------------------------
 	// MOVE STUFF AROUND
 	//----------------------------------------------------------------------------
 
-	let ejeY = vec3(0.0, 1.0, 0.0);
-	let R = rotate(rotAngle, ejeY);	
+	let ejeY = vec3(0.0, 1.0, 0.0), ejeX = vec3(1.0, 0.0, 0.0), ejeZ = vec3(0.0, 0.0, 1.0);
+	let R = [ rotate(rotAngle, ejeY), rotate(2*rotAngle, ejeY) ];
 
-	objectsToDraw[2].uniforms.u_model = translate(1.0, 1.0, 3.0);
-	objectsToDraw[2].uniforms.u_model = mult(objectsToDraw[2].uniforms.u_model, R);
-	
-	objectsToDraw[3].uniforms.u_model = translate(1.0, 0.0, 3.0);
-	objectsToDraw[3].uniforms.u_model = mult(R, objectsToDraw[3].uniforms.u_model);
-
-	objectsToDraw[4].uniforms.u_model = translate(-1.0, 0.0, -2.0);
-	objectsToDraw[4].uniforms.u_model = mult(R, objectsToDraw[4].uniforms.u_model);
+	var j = -1;
+	var k = 0.2;
+	var l = 1;
+	for (let i=2; i < 22; i++)
+	{
+		if(i % 2 == 0) { j++; }
+		objectsToDraw[i].uniforms.u_model = translate(1.0 + j, 0.0, 3.0);
+		objectsToDraw[i].uniforms.u_model = mult(rotate(rotAngle * l, ejeY/*ejes[i]*/), objectsToDraw[i].uniforms.u_model);
+		objectsToDraw[i].uniforms.u_model = mult(objectsToDraw[i].uniforms.u_model, rotate(rotAngle * l, ejeY/*ejes[i]*/));
+		l = l + k;
+	}
 	
 	//----------------------------------------------------------------------------
 	// DRAW
@@ -388,7 +484,6 @@ function render() {
     });	
     
 	rotAngle += rotChange;
-	lastFrameTime = currFrameTime;
 	camera();
 	
 	requestAnimationFrame(render);
@@ -424,7 +519,7 @@ function setUniforms(pInfo, uniforms) {
 	gl.uniformMatrix4fv(pInfo.uniformLocations.model, gl.FALSE, uniforms.u_model);
 }
 
-function setBuffersAndAttributes(pInfo, ptsArray, colArray) {
+function setBuffersAndAttributes(pInfo, ptsArray, color) {
 	// Load the data into GPU data buffers
 	// Vertices
 	var vertexBuffer = gl.createBuffer();
@@ -434,9 +529,5 @@ function setBuffersAndAttributes(pInfo, ptsArray, colArray) {
 	gl.enableVertexAttribArray( pInfo.attribLocations.vPosition );
 
 	// Colors
-	var colorBuffer = gl.createBuffer();
-	gl.bindBuffer( gl.ARRAY_BUFFER, colorBuffer );
-	gl.bufferData( gl.ARRAY_BUFFER,  flatten(colArray), gl.STATIC_DRAW );
-	gl.vertexAttribPointer( pInfo.attribLocations.vColor, 4, gl.FLOAT, gl.FALSE, 0, 0 );
-	gl.enableVertexAttribArray( pInfo.attribLocations.vColor );
+	gl.vertexAttrib4fv(pInfo.attribLocations.vColor, color);
 }
